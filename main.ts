0bb1abb52ce9a5d5bd545e76c08ba79089093758
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-07-17 11:38:34 +0900

    envLoader() & Checker func(progress)

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { tty } from "@cliffy/ansi/tty";
 import { Select } from "@cliffy/prompt";
 import "@std/dotenv/load";
 import { logger } from "npm:comodern";
@@ -5,18 +6,43 @@ import color from "npm:picocolors";
 
 const version = "0.1.0";
 
+function envLoader() {
+  //token
+  const tr = Deno.env.get("TOKEN");
+  if (!tr) {
+    logger.error("[envLoader] cannot find token from .env");
+    Deno.exit(1);
+  }
+  const token = tr?.includes(",") ? tr.split(",") : [tr];
+
+  //hostname
+  const hostname = Deno.env.get("HOSTNAME");
+  if (!hostname) {
+    logger.error("[envLoader] cannot find token from .env");
+    Deno.exit(1);
+  }
+
+  logger.log("[envLoader] Loaded all env");
+  return { token, hostname };
+}
+
 async function main() {
   logger.info(color.bgCyanBright(color.black(` UtilSky v${version} `)));
+  logger.info(color.gray("Initializing..."));
+  const env = envLoader();
+  logger.success(`Loaded (${env.token.length} tokens, host: ${env.hostname})`);
+
   /**
    * 1: Spam
    *
    * 2: Clean up acc
    *
-   * 3: Acc info
+   * 3: Acc checker
    *
    * 4: Achivements unlocker
    */
   const mode = await Select.prompt({
+    cbreak: true,
     message: "What do you want to do?",
     options: [
       {
@@ -28,7 +54,7 @@ async function main() {
         value: 2,
       },
       {
-        name: "Account Info",
+        name: "Account Checker",
         value: 3,
       },
       {
@@ -37,6 +63,7 @@ async function main() {
       },
     ],
   });
+
   if (mode === 1) {
     //spam
   }
@@ -44,7 +71,7 @@ async function main() {
     //cleanup acc
   }
   if (mode === 3) {
-    //accinfo
+    //acccheck
   }
   if (mode === 4) {
     //achivements
@@ -52,3 +79,9 @@ async function main() {
 }
 
 main();
+
+Deno.addSignalListener("SIGINT", () => {
+  tty.cursorLeft.eraseDown.cursorShow();
+  logger.info("Interrupted, exiting...");
+  Deno.exit(1);
+});
